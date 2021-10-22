8b7092c61f9d852a001eca98099be078b21140e6
Ysla <yslariala@gmail.com>
2021-10-21 22:27:35 -0300

    consoles logs and if-else

---
+++ consoles-log-and-if-else.js
@@ -0,0 +1,16 @@
+var vel = 60
+console.log(`velocidade do carro era ${vel} km/h`)
+if (vel > 60)
+{
+    console.log('voce ultrapassou a velocidade permitida. multado.')
+}
+console.log('dirija usando cinto de segurança')
+var país = 'Brasil'
+console.log(`nasceu em ${país}`)
+if (país == 'Brasil') {
+    console.log('brasileiro')
+}
+else {
+    console.log('estrangeiro')
+}
+
